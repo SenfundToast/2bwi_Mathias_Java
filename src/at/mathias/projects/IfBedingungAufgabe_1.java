78e602e64340b57a3c2d162dd2554760c6674ae4
mathias <mathias@beirer.com>
2022-03-17 16:13:17 +0100

    IfBedingung_1

--- src/at/mathias/projects/IfBedingungAufgabe_1.java
+++ src/at/mathias/projects/IfBedingungAufgabe_1.java
@@ -1,5 +1,23 @@
 package at.mathias.projects;
 
-public class IfBedingungAufgabe_1 {
+import java.util.Random;
 
+public class IfBedingungAufgabe_1 {
+    public static void main(String[] args) {
+        System.out.println("Test");
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        System.out.println(randomNumber);
+
+        if (randomNumber<20) {
+            System.out.println("Mini");
+        }
+        else if (randomNumber > 20 && randomNumber < 50) {
+            System.out.println("Medium");
+        }
+        else {
+            System.out.println("Large");
+            }
+    }
 }
