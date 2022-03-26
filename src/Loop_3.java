da9333db6feaa3c2e31e9f93044d4452f6c412ff
mathias <mathias@beirer.com>
2022-03-26 13:44:58 +0100

    Loop

--- src/Loop_3.java
+++
@@ -1,8 +0,0 @@
-public class Loop_3 {
-    public static void main(String[] args) {
-
-        for (int i = 2; i < 1000; i = i + 2) {
-            System.out.println(i);
-        }
-    }
-}
--- src/at/mathias/projects/Loop_2.java
+++ src/at/mathias/projects/Loop_2.java
@@ -2,6 +2,7 @@ package at.mathias.projects;
 
 public class Loop_2 {
     public static void main(String[] args) {
+
         for (int i = 1; i < 100; i++) {
             System.out.println(i);
         }
