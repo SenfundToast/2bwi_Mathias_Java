c3e1fc50fe220ded5115ba12563d4e55c98aaffd
mathias <mathias@beirer.com>
2022-03-26 13:13:21 +0100

    SwitchStatement_1

--- src/at/mathias/projects/SwitchStatement_1.java
+++ src/at/mathias/projects/SwitchStatement_1.java
@@ -1,4 +1,26 @@
 package at.mathias.projects;
 
+import java.util.Random;
+
 public class SwitchStatement_1 {
+    public static void main(String[] args){
+
+        Random random = new Random();
+        int randomNumber = random.nextInt(5);
+
+    switch (randomNumber) {
+        case 0:
+            System.out.println("is 0");
+            break;
+        case 1:
+            System.out.println("is 1");
+            break;
+        case 2:
+            System.out.println("is 2");
+            break;
+        default:
+            System.out.println("other value");
+            break;
+    }
+    }
 }
