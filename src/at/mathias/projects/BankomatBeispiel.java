ae77493419b2a88f0d8da47177956941f9deb241
mathias <mathias@beirer.com>
2022-04-21 16:26:33 +0200

    bankomat

--- src/at/mathias/projects/BankomatBeispiel.java
+++ src/at/mathias/projects/BankomatBeispiel.java
@@ -14,7 +14,11 @@ public class BankomatBeispiel {
         int selection = 0;
         int bankBalance = 0;
         Scanner scanner = new Scanner(System.in);
-        while ( selection!= CANCEL ) {
+
+        while (selection != CANCEL) {
+
+            System.out.println();
+
             System.out.println("Selektieren Sie die gewünschte Funktinon:");
 
             System.out.println(" 1.  Einzahlen");
@@ -27,17 +31,34 @@ public class BankomatBeispiel {
 
             System.out.println("Wähle eine Zahl aus:");
 
-        }
-        System.out.println();
 
-        while (selection!= CANCEL) {
-            if (selection == 1)
+            selection = scanner.nextInt();
+
+            if (selection == 1) {
                 System.out.println();
                 System.out.println("Geben Sie den Betrag ein den Sie einzahlen möchten:");
-            int depositedAmount = scanner.nextInt();
-            bankBalance = bankBalance + depositedAmount;
-                System.out.println("Kontostand nach Überweisung: " + bankBalance);
+                int amountPaid = scanner.nextInt();
+                bankBalance = bankBalance + amountPaid;
+                System.out.println("Sie haben " + bankBalance + " Euro eingezahlt!");
+
+            } else if (selection == 2) {
                 System.out.println();
+                System.out.println("Wieviel würden Sie gerne abheben?");
+                int amountwithdrawn = scanner.nextInt();
+                bankBalance = bankBalance - amountwithdrawn;
+                System.out.println();
+                System.out.println("Sie haben " + bankBalance + " Euro abgehoben");
+
+            } else if (selection == 3) {
+
+                System.out.println("Der Kontostand beträgt: " + bankBalance);
+
+            } else {
+                System.out.println();
+                System.out.println("Machine is stopping!");
+                break;
+            }
         }
     }
 }
+
