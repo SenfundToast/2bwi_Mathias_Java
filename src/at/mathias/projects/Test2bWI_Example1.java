29ffc276e2d1a3dae74d37ffdbf0cfff2dd576f1
mathias <mathias@beirer.com>
2022-06-03 14:07:04 +0200

    verbesserung (nicht fertig)

--- src/at/mathias/projects/Test2bWI_Example1.java
+++ src/at/mathias/projects/Test2bWI_Example1.java
@@ -7,12 +7,15 @@ public class Test2bWI_Example1 {
     public static void main(String[] args) throws IOException {
 
         Random random = new Random();
-        int randomNumber2 = random.nextInt(0, 1000);
-        int randomNumber = random.nextInt(0, 1000);
+        int randomNumber2 = random.nextInt(0, 1001);
+        int randomNumber = random.nextInt(0, 1001);
+
+        System.out.println("Erste Zufallszahl " + randomNumber2);
+        System.out.println("Zweite Zufallszahl " + randomNumber);
 
         int i;
         if (randomNumber < randomNumber2) {
-            for (i = randomNumber2; i < randomNumber; i--);
+            for (i = randomNumber2; i >= randomNumber; i--);
             System.out.println("Zahl: " + i);
 
         } else if (randomNumber2 > randomNumber) {
