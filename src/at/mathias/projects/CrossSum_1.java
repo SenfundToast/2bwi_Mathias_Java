601cf535adc0877f7b364628e85df97b0bb3eaad
mathias <mathias@beirer.com>
2022-09-13 11:28:56 +0200

    CrossSum_1-Einstieg(not finished)

---
+++ src/at/mathias/projects/CrossSum_1.java
@@ -0,0 +1,11 @@
+package at.mathias.projects;
+
+public class CrossSum_1 {
+    public static void main(String[] args) {
+    int[] crossSumCounter = new int[28];
+    for (int cnt = 0; cnt < 1001; cnt++){
+    String numberAsString = Integer.toString(cnt);
+
+    }
+    }
+}
