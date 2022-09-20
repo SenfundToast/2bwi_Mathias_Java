3935d1cbd05c2b5892b11b246da5b1a6e4952b03
mathias <mathias@beirer.com>
2022-09-20 10:52:27 +0200

    CrossSum - finished

--- src/CrossSum_2.java
+++ src/CrossSum_2.java
@@ -1,17 +1,16 @@
-package at.mathias.projects;
-
-public class CrossSum_1 {
+public class CrossSum_2 {
     public static void main(String[] args) {
-        for (int cnt = 0; cnt < 1001; cnt++){
+        for (int cnt = 0; cnt < 1001; cnt++) {
             String numberAsString = Integer.toString(cnt);
             char[] number = numberAsString.toCharArray();
 
             int sum = 0;
             for (int i = 0; i < number.length; i++) {
                 sum = sum + Character.getNumericValue(number[i]);
-             }
+            }
 
-            if (sum==15) {
+            int rest = sum % 7;
+            if (rest == 0) {
                 System.out.println("Number: " + cnt + " Quersumme: " + sum);
             }
         }
