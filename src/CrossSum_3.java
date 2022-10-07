d6b71c4d2a3e421cb84c6af52a152a0cc6f9bc86
mathias <mathias@beirer.com>
2022-10-07 16:46:46 +0200

    CrossSum3 - finished

--- src/CrossSum_3.java
+++ src/CrossSum_3.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class CrossSum_3 {
     public static void main(String[] args) {
 
@@ -7,20 +9,23 @@ public class CrossSum_3 {
             arr[i] = calculateCrossSum(i);
         }
 
-            int sum = 0;
-            for (int i = 0; i < number.length; i++) {
-                sum = sum + Character.getNumericValue(number[i]);
-                System.out.println("Quersumme von " + i +  ": " + sum);
-            }
+        int[] count = new int [arr.length];
 
-            int[] count = new int [arr.length];
-
-
-            for (int i = 0; i < count.length; i++) {
-
-            }
+        int temp;
+        for (int j : arr) {
+            temp = j;
+            count[temp]++;
         }
-    }
+
+            for (int i = 1; i < count.length; i++) {
+                if(count[i] == 1) {
+                    System.out.printf("Die Zahl %d ist %d mal vorhanden\n", i, count[i]);
+                } else if (count[i] >= 2) {
+                    System.out.printf("Die Zahl %d ist %d mal vorhanden\n", i, count[i]);
+                }
+            }
+
+        }
     public static int calculateCrossSum(int number) {
         int crossum = 0;
         while (0 != number) {
