8d253729f4d887f200768ea723c2a29d037f07ef
mathias <mathias@beirer.com>
2022-10-11 11:17:06 +0200

    CrossSum_4 - finished

--- src/at/mathias/projects/CrossSum_4.java
+++ src/at/mathias/projects/CrossSum_4.java
@@ -1,20 +1,31 @@
 package at.mathias.projects;
 
+
 public class CrossSum_4 {
+
     public static void main(String[] args) {
 
-        int summe = 0;
 
+            System.out.println("Gib mir eine Zahl");
 
-        for (int cnt = 0; cnt < 1001; cnt++) {
-            String numberAsString = Integer.toString(cnt);
-            char[] number = numberAsString.toCharArray();
+            for (int cnt = 0; cnt < 1001; cnt++) {
+                String numberAsString = Integer.toString(cnt);
+                char[] number = numberAsString.toCharArray();
 
-            int sum = 0;
-            for (int i = 0; i < number.length; i++) {
-                sum = sum + Character.getNumericValue(number[i]);
-                System.out.println("Quersumme von " + cnt + ": " + sum);
-            }
+                int sum = 0;
+                for (int i = 0; i < number.length; i++) {
+                    sum = sum + Character.getNumericValue(number[i]);
+                }
+
+                while (sum > 9){
+                    int temporary = sum;
+                    sum = 0;
+                    while (temporary > 0) {
+                        sum += temporary % 10;
+                        temporary /= 10;
+                    }
+                }
+                System.out.println("Number: " + cnt + " iterierteQuersumme: " + sum);
         }
     }
 }
