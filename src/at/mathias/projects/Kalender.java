a679e235981e175424ef01af5ab5c3d92bc17b2f
mathias <mathias@beirer.com>
2022-10-14 14:51:32 +0200

    Kalender String

--- src/at/mathias/projects/Kalender.java
+++ src/at/mathias/projects/Kalender.java
@@ -1,4 +1,13 @@
 package at.mathias.projects;
 
+import groovy.lang.GString;
+
 public class Kalender {
+    public static void main(String[] args) {
+
+    String[] wochenmonate = {"Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
+    String[] wochenkürzungen = {"MO", "DIE", "MI", "DO", "FR", "SA", "SO"};
+
+
+    }
 }
