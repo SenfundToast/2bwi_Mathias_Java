40a9cd43944f010802ff8602c7984e3593d49abf
mathias <mathias@beirer.com>
2022-10-18 11:04:23 +0200

    Kalender - nameOfWeekdays

--- src/at/mathias/projects/Kalender.java
+++ src/at/mathias/projects/Kalender.java
@@ -5,8 +5,20 @@ import groovy.lang.GString;
 public class Kalender {
     public static void main(String[] args) {
 
+
+
     //String[] namesOfMonths = {"Jänner", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember"};
     String[] nameOfWeekdays = {"MO", "DIE", "MI", "DO", "FR", "SA", "SO"};
 
+        for(int i = 0; i < nameOfWeekdays.length; i++) {
+            String separator= " | ";
+            String endSeparator = "";
+
+            if(i == 6) {
+                endSeparator = " |\n";
+            }
+            System.out.print(separator + nameOfWeekdays[i] + endSeparator);
+
+        }
     }
 }
