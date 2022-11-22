f5106f83641aa6b0cbbaeb8cb6dc282fd9d0d429
mathias <mathias@beirer.com>
2022-11-22 11:17:23 +0100

    Car-finished

--- src/at/mathias/projects/OO_Basics/car/Car.java
+++ src/at/mathias/projects/OO_Basics/car/Car.java
@@ -1,6 +1,13 @@
 package at.mathias.projects.OO_Basics.car;
 
 public class Car {
+    public String color;
+    public int fuelConsumption;
+    public String serielNumber;
+    public int fuelAmount;
 
+    public void drive(){
+        this.fuelAmount = this.fuelAmount - fuelConsumption;
+        System.out.println("I am driving");
     }
-}
+    }
