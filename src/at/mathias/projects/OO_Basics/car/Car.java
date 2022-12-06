ad729b760f82295085943d832b71f323f2c2226b
mathias <mathias@beirer.com>
2022-12-06 10:02:34 +0100

    OO_Basisc_Car

--- src/at/mathias/projects/OO_Basics/car/Car.java
+++ src/at/mathias/projects/OO_Basics/car/Car.java
@@ -3,11 +3,14 @@ package at.mathias.projects.OO_Basics.car;
 public class Car {
     public String color;
     public int fuelConsumption;
-    public String serielNumber;
+    public String serialNumber;
     public int fuelAmount;
 
-    public void drive(){
-        this.fuelAmount = this.fuelAmount - fuelConsumption;
+    public void drive(int amount){
+        this.fuelAmount -= fuelConsumption /100 * amount;
         System.out.println("I am driving");
     }
+    public void brake(){
+        System.out.println("I brake");
     }
+}
--- src/at/mathias/projects/OO_Basics/car/Main.java
+++ src/at/mathias/projects/OO_Basics/car/Main.java
@@ -2,16 +2,14 @@ package at.mathias.projects.OO_Basics.car;
 
 public class Main {
     public static void main(String[] args) {
-        int a = 7;
-
-
         Car c1 = new Car();
         c1.color ="Yellow";
         c1.fuelConsumption = 7;
-        c1.serielNumber="C1234";
+        c1.serialNumber ="C1234";
         c1.fuelAmount=70;
         System.out.println("Fuel: " +c1.fuelAmount);
-        c1.drive();
+        c1.drive(10);
         System.out.println("Fuel: " +c1.fuelAmount);
+        c1.brake();
     }
 }
