438071b929d51bdedefc959ae45f14150e459acb
mathias <mathias@beirer.com>
2022-12-06 11:34:00 +0100

    OO_car_setOfReception

--- src/at/mathias/projects/OO_Basics/car/Car.java
+++ src/at/mathias/projects/OO_Basics/car/Car.java
@@ -2,15 +2,31 @@ package at.mathias.projects.OO_Basics.car;
 
 public class Car {
     public String color;
-    public int fuelConsumption;
+    public double fuelConsumption;
     public String serialNumber;
-    public int fuelAmount;
+    public double fuelAmount;
+    public int maxFuelAmount;
+    public int amountOfReception;
 
-    public void drive(int amount){
-        this.fuelAmount -= fuelConsumption /100 * amount;
+
+    public void drive() {
+        this.fuelAmount -= this.fuelConsumption / 100;
         System.out.println("I am driving");
     }
-    public void brake(){
+
+    public void brake() {
         System.out.println("I brake");
+    }
+
+    public void turboBoost() {
+        if (this.fuelAmount >= 0.1 * this.maxFuelAmount) {
+            System.out.println("SuperBoostMode");
+        } else {
+            System.out.println("Not enough fuel to go to Superboost");
+        }
+    }
+    public void setOfReception(){
+
+        System.out.println("Tuuut!");
     }
 }
