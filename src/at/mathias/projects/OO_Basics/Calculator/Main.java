deb6ac2fbe290f719eda2d66ad7520b324a43fbd
mathias <mathias@beirer.com>
2023-03-24 15:07:24 +0100

    oo_standardCalculator-started

---
+++ src/at/mathias/projects/OO_Basics/Calculator/Main.java
@@ -0,0 +1,7 @@
+package at.mathias.projects.OO_Basics.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/at/mathias/projects/OO_Basics/Calculator/ScientificCalculator.java
@@ -0,0 +1,4 @@
+package at.mathias.projects.OO_Basics.Calculator;
+
+public class ScientificCalculator {
+}
