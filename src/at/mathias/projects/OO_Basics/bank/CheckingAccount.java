80652e5ef3bc5f39351086e80056b72edcbddbc6
mathias <mathias@beirer.com>
2023-04-18 10:54:38 +0200

    oo_bank_checkingAccount - ready

--- src/at/mathias/projects/OO_Basics/bank/CheckingAccount.java
+++ src/at/mathias/projects/OO_Basics/bank/CheckingAccount.java
@@ -1,6 +1,20 @@
 package at.mathias.projects.OO_Basics.bank;
 
 public class CheckingAccount {
+    private int limit;
 
+    public CheckingAccount(int limit) {
+        this.limit = limit;
+    }
 
+    @Override
+    public double withdraw(double amount) {
+        if((getBalance()-amount)< (limit * -1) ){
+            System.out.println("Withdrawn " + amount + " Euros");
+            return super.withdraw(amount);
+        } else {
+            System.out.println("Leider keine Deckung!");
+            return 0;
+        }
+    }
 }
---
+++ src/at/mathias/projects/OO_Basics/bank/LaendleAccount.java
@@ -0,0 +1,4 @@
+package at.mathias.projects.OO_Basics.bank;
+
+public class LaendleAccount {
+}
